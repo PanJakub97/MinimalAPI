4ff06e8cbac91b46fb43595e8e85d2157245186c
PanJakub97 <kuba.stasiak78@gmail.com>
2023-01-19 21:46:50 +0100

    minimal API

---
+++ ToDos.MinimalAPI/ToDO/ToDoRequest.cs
@@ -0,0 +1,103 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDos.MinimalAPI.ToDO
+{
+    public static class ToDoRequest
+    {
+        public static WebApplication RegisterEndPoints(this WebApplication app)
+        {
+            app.MapGet("/todos", ToDoRequest.GetAll)
+                .Produces<List<ToDo>>()
+                .WithTags("To Dos");
+
+            app.MapGet("/todos/{id}", ToDoRequest.GetById)
+                .Produces<ToDo>()
+                .Produces(StatusCodes.Status404NotFound)
+                .WithTags("To Dos");
+
+            app.MapPost("/todos", ToDoRequest.Create)
+                .Produces<ToDo>(StatusCodes.Status201Created)
+                .Accepts<ToDo>("application/json")
+                .WithTags("To Dos")
+                .WithValidator<ToDo>();
+
+            app.MapPut("/todos/{id}", ToDoRequest.Update)
+                .WithValidator<ToDo>()
+                .Produces<ToDo>(StatusCodes.Status204NoContent)
+                .Produces<ToDo>(StatusCodes.Status404NotFound)
+                .Accepts<ToDo>("application/json")
+                .WithTags("To Dos");
+
+            app.MapDelete("/todos/{id}", ToDoRequest.Delete)
+                .Produces<ToDo>(StatusCodes.Status204NoContent)
+                .Produces<ToDo>(StatusCodes.Status404NotFound)
+                .WithTags("To Dos")
+                .ExcludeFromDescription();
+
+            return app;
+        }
+
+        public static IResult GetAll(IToDoService service)
+        {
+            var todos = service.GetAll();
+            return Results.Ok(todos);
+        }
+
+        public static IResult GetById(IToDoService service, Guid id)
+        {
+            var todo = service.GetById(id);
+            if(todo == null)
+            {
+                return Results.NotFound(); //404
+            }
+
+            return Results.Ok(todo); //200
+        }
+
+        public static IResult Create(IToDoService service, ToDo toDo)
+        {
+            //var validationResult = validator.Validate(toDo);
+            //if (!validationResult.IsValid)
+            //{
+            //    return Results.BadRequest(validationResult.Errors);
+            //}
+
+            service.Create(toDo);
+
+            return Results.Created($"/todos/{toDo.Id}", toDo);
+        }
+
+        public static IResult Update(IToDoService service, Guid id, ToDo toDo)
+        {
+            //var validationResult = validator.Validate(toDo);
+            //if (!validationResult.IsValid)
+            //{
+            //    return Results.BadRequest(validationResult.Errors);
+            //}
+
+            var todo = service.GetById(id);
+            if (todo == null)
+            {
+                return Results.NotFound(); //404
+            }
+
+            service.Update(todo);
+
+            return Results.NoContent(); //204
+        }
+
+        public static IResult Delete(IToDoService service, Guid id)
+        {
+            var todo = service.GetById(id);
+            if (todo == null)
+            {
+                return Results.NotFound(); //404
+            }
+
+            service.Delete(id);
+            return Results.NoContent(); //204
+        }
+    }
+}
